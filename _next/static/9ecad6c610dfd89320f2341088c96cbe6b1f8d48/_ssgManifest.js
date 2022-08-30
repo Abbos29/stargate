4d212d44b154a1dea5e386c4d27eb909520e112b
Abbos29 <78086415+Abbos29@users.noreply.github.com>
2022-08-30 13:42:15 +0500

    Add files via upload

---
+++ _next/static/9ecad6c610dfd89320f2341088c96cbe6b1f8d48/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F404","\u002Fstake\u002F[stakeId]","\u002Fpool\u002F[poolId]\u002Fremove","\u002Fpool\u002F[poolId]\u002Foptions","\u002Fpool\u002F[poolId]\u002Fadd","\u002Ffarm\u002F[farmId]\u002Fremove","\u002Fpool\u002F[poolId]","\u002Ffarm\u002F[farmId]\u002Fadd","\u002Ffarm\u002F[farmId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
